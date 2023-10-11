8268fcd7b7a664d962a865393bdb8e5bc40a763b
Mikk-Markus Soomre <mmsoomre@miyaow.net>
2023-10-11 19:24:50 +0300

    add search and more HTML <-> JS consistency

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function createTableColumnInto(rowElement, value) {
-    let tableBody = document.getElementById("usersTableBody")
-    let col = document.createElement("td")
+    const tableBody = document.getElementById("usersTableBody")
+    const col = document.createElement("td")
     col.classList.add("table-element")
     col.innerText = value
 
@@ -8,8 +8,8 @@ function createTableColumnInto(rowElement, value) {
 }
 
 function createTableRowFromUser(user) {
-    let tableBody = document.getElementById("usersTableBody")
-    let row = document.createElement("tr")
+    const tableBody = document.getElementById("usersTableBody")
+    const row = document.createElement("tr")
     createTableColumnInto(row, user.username)
     createTableColumnInto(row, user.firstname)
     createTableColumnInto(row, user.surname)
@@ -46,14 +46,55 @@ function fetchFakeUsers() {
     )
 }
 
-function loadUsers(users) {
-    for (const index in users) {
+function createEntries(users) {
+    const tableBody = document.getElementById("usersTableBody")
+    tableBody.replaceChildren()
+
+    for (const index in users)
         createTableRowFromUser(users[index])
+}
+
+let knownUsers
+fetchFakeUsers()
+    .then((users) => {
+        knownUsers = users
+        createEntries(users)
+    })
+    .catch((err) => {
+        document.getElementById("errorMessage").innerText = toString(err)
+    })
+
+function searchUsersAction() {
+    const expectedUsername = document.getElementById("usernameSearch").value.toLowerCase()
+    const expectedFirstName = document.getElementById("firstnameSearch").value.toLowerCase()
+    const expectedSurname = document.getElementById("surnameSearch").value.toLowerCase()
+    const noResultText = document.getElementById("noResultText")
+    const resultCountText = document.getElementById("resultCountText")
+    const resultCount = document.getElementById("resultCount")
+    const usersTable = document.getElementById("usersTable")
+
+    let results = []
+
+    for (const user of knownUsers) {
+        let matches = true
+        if (expectedUsername && expectedUsername.trim() !== "")
+            if (!user.username.toLowerCase().includes(expectedUsername))
+                matches = false
+        if (expectedFirstName && expectedFirstName.trim() !== "")
+            if (!user.firstname.toLowerCase().includes(expectedFirstName))
+                matches = false
+        if (expectedSurname && expectedSurname.trim() !== "")
+            if (!user.surname.toLowerCase().includes(expectedSurname))
+                matches = false
+        if (matches)
+            results.push(user)
     }
+
+    resultCount.innerText = results.length
+
+    usersTable.hidden = (results.length == 0)
+    resultCountText.hidden = (results.length == 0)
+    noResultText.hidden = (results.length > 0)
+
+    createEntries(results)
 }
-
-function searchUsers() {
-
-}
-
-fetchFakeUsers().then((users) => loadUsers(users))
